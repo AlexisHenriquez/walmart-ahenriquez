bb269c219f44541022ae27afb56d091fc1b913f6
Alexis Henríquez <alexis.henriquezb@gmail.com>
2020-11-06 13:25:56 -0300

    Changed .net core solution

---
+++ walmart-ahenriquez/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace walmart_ahenriquez.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
